8d13f408b7448ad1fd3f6b1f0d666e4c702b7ba9
Thanushan Navasothy <thanush.navasot@gmail.com>
2026-05-09 16:31:02 +0100

    IntroductionPage

--- frontend/app/components/UserInformationOnboard/UserInformationOnboard.tsx
+++ frontend/app/components/UserInformationOnboard/UserInformationOnboard.tsx
@@ -1,5 +1,5 @@
 import "./UserInformationOnboard.css";
 
 export const UserInformationOnboard = () => {
-  return <div className="user-information-onboard-container">test</div>;
+  return <div className="user-information-onboard-container"></div>;
 };
